846ae5e47ea90043cdb0e6914631437efaec8aff
rayd25051992 <rayd_250592@outlook.com>
2013-12-05 02:35:55 +0000

    LOC Counted. Edit Homepage created. Need to change UI on other edit ages and LHS Menu. Header and footer edit UI needs changed as well

--- secure/cust_charter.php
+++ secure/cust_charter.php
@@ -24,11 +24,38 @@
         <div id="contentwrap">
         <div id="content">
      
-		<h1 style="margin-left:10px" > Home </h1>
+		<h1 style="margin-left:10px" > Customer Charter </h1>
 		
-		<p>Welcome to Raymond's PC's. Here you will find the best deal's on laptops and computers in the country. Keep an eye out for our seasonal offers.</p>
-		<p>With free onsite parking and excellent advice when buying a PC or laptop, you wont want to buy anywhere else!</p>
+		<p style="margin-left:10px">Raymond's PC's staff are committed to providing first class customer service and ensuring that you are happy with every part of our services. 
+		We have looked after thousands of customers and have a very loyal customer base. 
+		We are always looking to improve our service so please get in touch with any ideas or suggestions!</p>
 		
+		<h2 style="margin-left:10px" >1. Provide a First Class Customer Service</h2>
+		
+	    <p style="margin-left:10px">Raymond's PC's staff understand the importance of ensuring that they arrive at your home or business as quickly as possible and at the time agreed.
+	     We aim to repair 99% of issues without having to take any equipment away. 
+	     In the rare event that any equipment that is taken away for repair or further diagnosis, we will provide you with an expectation of when it will be returned.
+Where spare parts need to be sourced, your PC PAL Engineers aim to carry an extensive range of spare parts. 
+If we need to order specific parts, we will contact you to gain authorisation (with estimated time of arrival) for delivery upon acceptance of our quotation for parts.</p>
+
+<h2 style="margin-left:10px" >2. Prompt and Clear Communication </h2>
+		
+	    <p style="margin-left:10px">Your PC PAL Engineers understand the importance of answering your call and aim to answer all calls within 5 rings.
+	     If we are unable to answer immediately (as we may be speaking to another customer), then we will return your call within the hour. 
+	    We will also provide prompt replies via email.</p>
+	    
+	    <h2 style="margin-left:10px" >3. Clear and Simple Pricing</h2>
+		
+	    <p style="margin-left:10px">Your PC PAL Engineers aim to provide clear pricing information before commencing any work for you and will provide 
+	    a fixed price for the supply of new or replacement spare parts in advance for your authorisation.</p>
+	    
+	     <h2 style="margin-left:10px" >4. Payment and Receipts</h2>
+		
+	    <p style="margin-left:10px">Your PC PAL Engineers aim to ensure we provide convenient payment methods including Cash, Company Cheque, Debit and Credit cards.
+	     Payments will be taken via our secure website and a full receipt or invoice will be provided..</p>
+
+
+
 
         
         </div>
--- secure/index.php
+++ secure/index.php
@@ -4,7 +4,7 @@
 <?php include 'header.php'; ?>
 <title>Raymond's PC's</title>
 
-<link rel="stylesheet" type="text/css" href="styles/css.css" />
+<link rel="stylesheet" type="text/css" href="../styles/css.css" />
 </head>
 <body style="background-color:silver">
     <div id="wrapper">
@@ -24,12 +24,65 @@
         <div id="contentwrap">
         <div id="content">
      
-		<h1 style="margin-left:10px" > Home </h1>
+		<h1 style="margin-left:10px" > Edit Menu </h1>
 		
-		<p>Welcome to Raymond's PC's. Here you will find the best deal's on laptops and computers in the country. Keep an eye out for our seasonal offers.</p>
-		<p>With free onsite parking and excellent advice when buying a PC or laptop, you wont want to buy anywhere else!</p>
+			<table style="width: 100%; border:thick">
+			
+				<tr>
+					<td style="text-align:center"><a href="about.php"><input id="gobutton" type="submit" value="About the Company" /></a></td>
+					<td style="text-align:center">Edit the "About the Company Page"</td>
+				</tr>
+				<tr>
+					<td style="text-align:center"><a href="company_history.php"><input id="gobutton" type="submit" value="Company History" /></a></td>
+					<td style="text-align:center">Edit the "Company History"</td>
+				</tr>
+				<tr>
+					<td style="text-align:center"><a href="cust_charter.php"><input id="gobutton" type="submit" value="Customer Charter" /></a></td>
+					<td style="text-align:center">Edit the "Customer Charter"</td>
+				</tr>
+				<tr>
+					<td style="text-align:center"><a href="laptops.php"><input id="gobutton" type="submit" value="Laptops" /></a></td>
+					<td style="text-align:center">Edit the "Laptops" Page</td>
+				</tr>
+				<tr>
+					<td style="text-align:center"><a href="desktops.php"><input id="gobutton" type="submit" value="Desktops" /></a></td>
+					<td style="text-align:center">Edit the "Desktops" Page</td>
+				</tr>
+				<tr>
+					<td style="text-align:center"><a href="contact.php"><input id="gobutton" type="submit" value="Contact Us" /></a></td>
+					<td style="text-align:center">Edit the "Contact Us" Page</td>
+				</tr>
+				<tr>
+					<td style="text-align:center"><a href="header.php"><input id="gobutton" type="submit" value="Header" /></a></td>
+					<td style="text-align:center">Edit the Header</td>
+				</tr>
+				<tr>
+					<td style="text-align:center"><a href="footer.php"><input id="gobutton" type="submit" value="Footer" /></a></td>
+					<td style="text-align:center">Edit the Footer</td>
+				</tr>
+						</table>
+		
+		<table>
+		
+		
+		</table>
+		
+		
+		
+	
+	    
+	   
+	
 		
 
+
+     
+
+
+
+
+
+        
         
         </div>
         </div>
